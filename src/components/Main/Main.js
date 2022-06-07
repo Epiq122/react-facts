ae9029cc01d401c9614faf589847ec525a30769b
Epiq122 <l2ob122@gmail.com>
2022-06-06 21:57:06 -0700

    started styling and working on main

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,8 +1,16 @@
+import './Main.css';
 function Main() {
   return (
-    <div>
-      <h1>This is the Main Component</h1>
-    </div>
+    <main className='container'>
+      <h1 className='main-header'>Fun facts about React</h1>
+      <ul className='main-list'>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprice apps,including mobile apps</li>
+      </ul>
+    </main>
   );
 }
 
